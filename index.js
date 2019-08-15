eb4d263393edb119453a47da5f5db7b3170e62b1
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-15 02:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,3 +26,9 @@ function driversByName(drivers){
     return a.name.localeCompare(b.name);
   });
 }
+
+function totalRevenue(drivers){
+    return drivers.reduce(function (total, driver){
+        return driver.revenue + total
+    }, 0)
+  }
